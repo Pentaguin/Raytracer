0b6261fed4d39d3116093ca56d4e6f091fff3467
Pentaguin <>
2026-07-01 08:04:16 +0200

    Extract some color related code from Camera.java to Color.java

--- backend/src/main/java/com/example/raytracer/raytracer/Camera.java
+++ backend/src/main/java/com/example/raytracer/raytracer/Camera.java
@@ -59,7 +59,7 @@ public class Camera {
         double pixelScale = 1.0 / samplesPerPixel;
         pixelColor = pixelColor.multiply(pixelScale);
 
-        image.setRGB(i, j, toRGB(pixelColor));
+        image.setRGB(i, j, new Color().toRGB(pixelColor));
       }
     }
 
@@ -83,31 +83,6 @@ public class Camera {
   private Vec3 sampleSquare() {
     // Returns the vector to a random point in the [-.5,-.5]-[+.5,+.5] unit square.
     return new Vec3(randomDouble() - 0.5, randomDouble() - 0.5, 0);
-  }
-
-  // Translate the [0,1] component values to the byte range [0,255].
-  private int toRGB(Vec3 pixelColor) {
-    int colorScale = 256;
-    Interval intensity = new Interval(0.000, 0.999);
-
-    // Apply a linear to gamma transform for gamma 2
-    double r = linearToGamma(pixelColor.x);
-    double g = linearToGamma(pixelColor.y);
-    double b = linearToGamma(pixelColor.z);
-
-    int ir = (int) (colorScale * intensity.clamp(r));
-    int ig = (int) (colorScale * intensity.clamp(g));
-    int ib = (int) (colorScale * intensity.clamp(b));
-
-    return (ir << 16) | (ig << 8) | ib;
-  }
-
-  public static double linearToGamma(double linearComponent)
-  {
-    if (linearComponent > 0)
-      return Math.sqrt(linearComponent);
-
-    return 0;
   }
 
   private Vec3 rayColor(Ray r, int depth, Hittable world) {
